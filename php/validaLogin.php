210f96fcbf4973562eb5e5b385d8a91603119e59
JonathanSouzaS <johnny17.souza@gmail.com>
2021-11-09 22:27:42 -0300

    validação

--- php/validaLogin.php
+++ php/validaLogin.php
@@ -5,17 +5,12 @@
     define('USUARIO','root');
     define('SENHA', '');
     define('DB','animadots');
-
     $conexao = mysqli_connect(HOST, USUARIO, SENHA, DB) or die('Não foi possível conectar.');
-
     $usuario = mysqli_real_escape_string($conexao, $_POST['login']);
     $senha = mysqli_real_escape_string($conexao, $_POST['senha']);
-
     $query = "select * from professoresacesso where login = '{$usuario}' and senha = md5('{$senha}')";
     $resultado = mysqli_query($conexao, $query);
-
     $row = mysqli_num_rows($resultado);
-
     if ($row == 1) {
         $_SESSION["logado"] = TRUE;
         $_SESSION["user"] = $_POST["login"];
